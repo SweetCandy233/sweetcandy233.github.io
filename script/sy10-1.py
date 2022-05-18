535f5f91156c09291baf4114acf49377e6776ed3
SweetCandy233 <68184088+SweetCandy233@users.noreply.github.com>
2022-05-18 21:52:03 +0800

    Add files via upload

--- script/sy10-1.py
+++ script/sy10-1.py
@@ -1,1 +1,20 @@
+'''
+Sy10-1 求出所有的水仙花数，保存到Arms.txt文件。
+'''
 
+def armstrong():
+    addupsum = 0
+    f = open('Arms.txt', 'w', encoding='utf-8')
+    for i in range(100,1000):
+        num = str(i)
+        for j in range(len(num)):
+            addupsum += int(num[j])**3
+        if addupsum == int(num):
+            addupsum = 0
+            f.write('{}\n'.format(num))
+        else:
+            addupsum = 0
+            
+    f.close()
+
+armstrong()
